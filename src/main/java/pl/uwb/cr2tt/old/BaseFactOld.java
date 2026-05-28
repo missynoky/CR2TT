acfc0ee1a7488e1b595bcaf8d884fcc3206cd6b3
missynoky <106540931+missynoky@users.noreply.github.com>
2026-05-28 16:41:59 +0200

    Delete src/main/java/pl/uwb/cr2tt/old directory
    remove old package from repository

--- src/main/java/pl/uwb/cr2tt/old/BaseFactOld.java
+++
@@ -1,7 +0,0 @@
-package pl.uwb.cr2tt.old;
-
-import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.RDFNode;
-import org.apache.jena.rdf.model.Resource;
-
-public record BaseFactOld(Resource subject, Property predicate, RDFNode object) {}
