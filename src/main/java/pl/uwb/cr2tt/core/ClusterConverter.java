2ecd9be44b3fbcbf002596ce5697f924154bbd95
missy_noky <magdalena.stankiewicz14@gmail.com>
2026-05-28 19:14:52 +0200

    add DIRECT_TRIPLE mode

--- src/main/java/pl/uwb/cr2tt/core/ClusterConverter.java
+++ src/main/java/pl/uwb/cr2tt/core/ClusterConverter.java
@@ -1,6 +1,9 @@
 package pl.uwb.cr2tt.core;
 
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.rdf.model.*;
+import org.apache.jena.vocabulary.RDF;
 import pl.uwb.cr2tt.model.Cluster;
 import pl.uwb.cr2tt.model.ConversionMode;
 
@@ -73,6 +76,19 @@ public class ClusterConverter {
                 break;
 
             case DIRECT_TRIPLE:
+                Node tripleNode = NodeFactory.createTripleTerm(
+                        s.asNode(),
+                        p.asNode(),
+                        o.asNode()
+                );
+
+                Resource tripleTermSubj = outGraph.asRDFNode(tripleNode).asResource();
+
+                outGraph.add(tripleTermSubj, RDF.type, RDF.Statement);
+
+                for (Statement metaStmt : cluster.getMetadata()) {
+                    outGraph.add(tripleTermSubj, metaStmt.getPredicate(), metaStmt.getObject());
+                }
                 break;
 
             default:
